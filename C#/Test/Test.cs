c66ff129715f420ffeede89552428e66beacc881
nick <nmarble@uccs.edu>
2017-08-22 20:14:16 -0600

    Added C# folder and test

---
+++ C#/Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("C# wishes it was java!");
+            Console.ReadLine();
+        }
+    }
+}
